aded04ed2cc3ba0191c6f8dfac0f175241619cad
papa2415 <167390811+papa2415@users.noreply.github.com>
2024-09-07 23:44:10 +0800

    gh-page

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/vue-2024';
-  plugins: [
-    vue(),
-  ],
+  base: '/vue-2024/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
